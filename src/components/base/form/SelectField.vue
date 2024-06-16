4f1326a425b8e89c56aef295f6efb38c0d0b1edf
d4ism3l <d4ism3l@gmail.com>
2024-06-16 01:54:07 -0400

    Terminando SelectField.vue se adicionaron caracteristicas para cargar opciones desde una url o pasarlas en la data.

--- src/components/base/form/SelectField.vue
+++ src/components/base/form/SelectField.vue
@@ -1,18 +1,48 @@
 <template>
-  div class="full-width">
-  <q-select ref="refSelect" :name="props.name" :label="props.label" :v-bind="fieldOptions"
-            :rules="fieldRules"
-            :option-value="props.optionValue" :options="OptionsFilters" :model-value="selectedModel"
-            @filter="(val, update, abort) => filterFn(val, update, abort)" :options-cover="!useInput ? true : false"
+  <!--
+    SelectField
+      props.name --- name
+      props.label --- label
+      props.options --- options [resto de opciones] estas tienen prioridad sobre las opciones generales.
+        props.options.optionLabel --- [mapear un label desde una data] || default label
+        props.options.optionValue --- [mapear un valor desde una data] || default value
+      props.data ---- Permite pasar la data se toma id como valor y label como name por defecto.
+      props.data_url --- Se pasa la url completa. Permite obtener los datos a partir de un endpoint y
+        luego podemos mapear value por options.optionValue (default: id) y label por options.optionLabel
+
+
+  -->
+  <q-select ref="refEl" :name="props.name" :label="props.label" v-bind="fieldOptions" :loading="loading"
+            :rules="fieldRules" multiple
+            :option-label="props.options?.optionLabel ?? 'label'"
+            :option-value="props.options?.optionValue ?? props.data_url.length ? 'id': 'value'"
+            :option-disable="props.options?.optionDisable||'disable'"
+            :options="OptionsFilters" :model-value="selectedModel"
+            transition-hide="flip-up"
+            transition-show="flip-down"
+            @click="handleClick()"
+            @popup-show="handleClick()"
+            @filter="(val, update, abort) => filterFn(val, update, abort)"
             :reset-validation="resetValidation ?? null" @update:model-value="(val) => updateModel(val)" :error="error"
-            :stack-label="props.stackLabel" @remove="({ index, value }) => onRemove(index, value)">
+            @remove="({ index, value }) => onRemove(index, value)">
+
+    <template #loading>
+      <q-item>
+        <q-item-section class="text-uppercase text-subtitle1">
+          {{ $t('labels.loading') || 'Loading' }}
+        </q-item-section>
+        <q-item-section avatar>
+          <q-spinner-facebook size="xs"></q-spinner-facebook>
+        </q-item-section>
+      </q-item>
+    </template>
     <template v-slot:no-option>
       <q-item>
         <q-item-section avatar>
           <q-icon name="mdi-alert-outline"/>
         </q-item-section>
         <q-item-section class="text-grey">
-          {{ $t('components.select.noptions') }}
+          {{ $t('noOptions') }}
         </q-item-section>
       </q-item>
     </template>
@@ -23,7 +53,7 @@
 </template>
 
 <script setup>
-import {computed, onBeforeMount, onMounted, ref} from "vue";
+import {computed, onBeforeMount, onMounted, ref, watch} from "vue";
 import {forms} from "src/config/theme/forms";
 import {required} from "src/services/validators";
 import {useQuasar} from "quasar";
@@ -45,15 +75,18 @@ const props = defineProps({
     type: String,
     required: true
   },
-  data_url: {type: String},
+  emitValues: {
+    type: Boolean,
+    default: true
+  },
+  preload: {
+    type: Boolean,
+    required: false
+  },
+  data_url: {type: String, required: false},
   data: {
     type: Object,
-    default: () => ({})
-  },
-  mapData: {
-    type: Object,
-    default: () => {
-    }
+    required: false
   },
   options: {
     type: Object,
@@ -64,57 +97,93 @@ const props = defineProps({
 const emits = defineEmits(["update", "error"])
 
 const $q = useQuasar();
-const refEl = ref()
+const refEl = ref(null)
 let textValue = ref("")
 const rules = ref([])
-const fieldOptions = {...forms.text, ...props.options}
-const fieldRules = {...rules.value, ...props.options?.rules ?? []}
+const fieldOptions = ref({...forms.select, ...props.options})
+const fieldRules = [...rules.value, ...props.options?.rules ?? []]
+const loading = ref(false)
 /** Variables */
 const selectedModel = ref()
-const refSelect = ref()
-const Options = ref([]);
-const error = ref(false);
-/** Options */
+const Options = ref([]);  //Total de <options>
 let OptionsFilters = ref([]);
 
-onBeforeMount(() => {
-  if (props.field && props.field?.options) {
-    if (props.field.required) {
-      rules.value.push(required)
+const error = ref(false);
+/** Options */
 
-    }
-    if (props.field.options.min && !Number.isNaN(props.field.options.min)) {
-      rules.value.push(minLength(field?.options.min))
-    }
-    if (props.field.options.max && !Number.isNaN(props.field.options.max)) {
-      rules.value.push(maxLength(props.field.options.max))
-    }
+onBeforeMount(async () => {
+  if (props.data_url.length && props.preload) {
+    const data = await getDataFromUrl()
+    Options.value = [...data];
+    setLoading();
   }
+  if (props.data && !props.data_url) {
+    Options.value = [...props.data];
+  }
+
+  if (fieldOptions.value?.required) {
+    rules.value.push(required)
+  }
+  
+  if (props.options?.value)
+    selectedModel.value = props.options?.value
+
+  if (props.modelValue.length > 0)
+    selectedModel.value = props.modelValue
+
 })
 onMounted(() => {
-  if (props.options && props.options?.value) {
-    textValue.value = props.options.value
-  }
-  if (props.modelValue.length > 0)
-    textValue.value = props.modelValue
+  selectedModel.value = refEl.value?.multiple ? [] : null
+
 
   // console.log("obj: ", textField.value)
 })
 
+async function getDataFromUrl() {
+  if (props.data_url.length) {
+    setLoading(true);
+    try {
+      const response = await fetch(props.data_url);
+      return response.json()
+    } catch (e) {
+      setLoading();
+
+      console.error(e);
+    }
+
+  }
+}
+
+async function handleClick() {
+  if (!props.preload && props.data_url.length) {
+    const data = await getDataFromUrl()
+    Options.value = [...data];
+    setLoading();
+
+  }
+}
+
+function setLoading(state = false) {
+  loading.value = state;
+}
+
 function updateModel(val) {
-  if (!props.multiple)
+
+  if (!props.multiple) {
     selectedModel.value = val
-  else
+
+  } else {
     selectedModel.value = val
+
+  }
 
 
   if (selectedModel.value) {
-    refSelect.value.resetValidation()
-    emits('hasError', false)
+    refEl.value.resetValidation()
   } else {
-    emits('hasError', true)
+    emits('error', true)
   }
-  emits('updateModelValue', val)
+  emits('update', val)
 }
 
 function onRemove(index, value) {
@@ -129,12 +198,12 @@ function onRemove(index, value) {
 }
 
 function validate() {
-  refSelect.value.validate()
+  refEl.value.validate()
 }
 
 function resetValidation() {
   error.value = false
-  refSelect.value.resetValidation()
+  refEl.value.resetValidation()
 }
 
 function reset() {
@@ -143,21 +212,59 @@ function reset() {
   } else {
     selectedModel.value = ''
   }
-  refSelect.value.reset()
+  refEl.value.reset()
   resetValidation()
 
 }
 
 function update(val) {
-  textValue.value = val
-  emits('update', textValue.value)
+  selectedModel.value = val
+  emits('update', selectedModel.value)
+}
+
+function filterFn(val, update, abort) {
+  update(
+    (sel) => {
+      if (val === '') {
+        OptionsFilters.value = []
+        Options.value.forEach((opt) => OptionsFilters.value.push(opt));
+        return;
+      } else {
+        const needle = val.toLowerCase();
+        //if (typeof (v.value) === 'string')
+        OptionsFilters.value = Options.value.filter(
+          (v) => v[props.options?.optionLabel || 'label']?.toLowerCase().indexOf(needle) > -1
+        );
+
+      }
+    },
+    // "ref" is the Vue reference to the QSelect
+    (refEl) => {
+      if (val !== '' && refEl.options?.length > 0 && refEl.getOptionIndex() === -1) {
+        refEl.moveOptionSelection(1, true); // focus the first selectable option and do not update the input-value
+        refEl.toggleOption(refEl.options[refEl.optionIndex], true); // toggle the focused option
+      }
+
+    }
+  );
+}
+
+function abortFilterFn() {
+  // console.log('delayed filter aborted')
 }
 
 const value = computed(() => {
-  return textValue.value
+  return selectedModel.value
 })
+
+
+watch(() => Options.value, (new_opts) => {
+  if (!props.preload && props.data_url.length)
+    OptionsFilters.value = [...new_opts]
+})
+
 defineExpose({
-  reset, validate, resetValidation, value
+  refEl, reset, validate, resetValidation, value
 })
 
 </script>
