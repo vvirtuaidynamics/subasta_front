acfdd4fe7d0372b3dcaf4e288bc56f57aebc9ebf
d4ism3l <d4ism3l@gmail.com>
2024-07-17 00:24:11 -0400

    Se trabaja en el servicio para consumir el estado.

--- src/composables/useApp.js
+++ src/composables/useApp.js
@@ -43,10 +43,38 @@ export function useApp() {
   function handleDarkMode(darkValue) {
     let data = {dark: darkValue}
     let config = user && configuration
-      ? {...configuration, ...{dark: darkValue}}
+      ? {...configuration.value, ...{dark: darkValue}}
       : {}
     data = {...data, ...{configuration: config}}
     setAppState(data);
+  }
+
+  function navigateTo(payload, $router = null, $route = null) {
+    if (payload && typeof payload === "string") {
+      if (payload.startsWith("http") || payload.startsWith("https")) {
+        window.open(payload, "_blank");
+      }
+      if (payload.startsWith("#")) {
+        document.getElementById(payload).scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+          inline: 'nearest'
+        });
+      }
+      if ($router) {
+        $router.push({name: payload}) // por defecto
+      }
+    }
+    if ($router && $route)
+      if (payload && typeof payload === "object") {
+        const path = payload.path;
+        if (path && $route.path !== path) {
+          $router.push(path);
+        }
+        if (payload.name) $router.push({name: payload.name});
+        if (payload.path) $router.push({path: payload.path});
+      }
+
   }
 
 
@@ -75,6 +103,7 @@ export function useApp() {
     // Methods
     setAppState,
     resetAppState,
-    handleDarkMode
+    handleDarkMode,
+    navigateTo
   };
 }
