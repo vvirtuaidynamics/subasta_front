dd3e16a593bfacccba15cb85c3774f5a985ef7e1
d4ism3l <d4ism3l@gmail.com>
2024-08-13 06:04:17 -0400

    feat: Add component PhoneField.

---
+++ src/components/base/form/PhoneField.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-tel-input v-model="phone" :search="true" lo default-country="ES" strictness="FULL" v-bind="props.options"
+  ></q-tel-input>
+</template>
+
+<script setup>
+import {QTelInput} from 'quasar-ui-q-tel-input'
+import {ref, watch} from "vue";
+
+defineOptions({
+  name: "PhoneField",
+});
+
+const props = defineProps({
+  modelValue: {type: String, required: false},
+  options: {
+    type: Object, default: () => {
+    }
+  }
+})
+
+const phone = ref('')
+const emits = defineEmits(['update'])
+
+const reset = () => phone.value = ''
+
+watch(() => phone.value, (newValue) => {
+  emits('update', newValue);
+
+})
+
+</script>
+<style src="quasar-ui-q-tel-input/dist/index.css"></style>
+<style scoped lang="scss">
+</style>
