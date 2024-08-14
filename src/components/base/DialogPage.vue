0545e975ff5b79c63fc749164a0c6aa32a060900
d4ism3l <d4ism3l@gmail.com>
2024-08-14 07:31:33 -0400

    fix: Se crea y modifica componentes Dialog para el trabajo con los forms

--- src/components/base/DialogPage.vue
+++ src/components/base/DialogPage.vue
@@ -1,53 +1,53 @@
 <template>
   <div id="QDialog" class="no-scrollbar">
     <q-dialog
-      v-model="pageDialog.showDialog"
-      :maximized="pageDialog.maximized"
-      :persistent="pageDialog.persistent"
+      v-model="showDialog"
+      :maximized="isMaximized"
+      :persistent="props.persistent"
       class="dialog"
-      full-height
-      full-width
+      :position="props.position"
       style="background-color: #1d1d1d99; border: none"
       @hide="handleClose()"
     >
       <q-card
         class="full-width no-margin no-padding"
-        style="border-radius: 15px 15px 15px 15px; border: none"
+        style="border-radius: 15px 15px 15px 15px; border: none; "
+        :style="!isMaximized? 'min-width: calc(100vw * 0.7)':'min-width: calc(100vw )'"
       >
         <q-bar class="">
-          <q-icon :name="pageDialog.icon ?? 'mdi-form-select'" size="sm"/>
+          <q-icon :name="props.icon" size="sm"/>
           <span class="q-ml-xs text-body1 text-bold text-uppercase">{{
-              pageDialog.title ?? $t("defaultDialogTitle")
+              props.title ?? $t("defaultDialogTitle")
             }}</span>
           <q-space/>
           <div class="q-gutter q-gutter-x-xs">
             <q-btn
-              v-if="pageDialog.maximizedBtn"
+              v-if="props.buttons.includes('maximize')"
               :class="
-                !pageDialog.maximized
+                !props.maximized
                   ? 'bg-indigo-8 text-white'
                   : 'bg-teal-8 text-negative'
               "
               :icon="
-                !pageDialog.maximized
+                !props.maximized
                   ? 'mdi-window-maximize'
                   : 'mdi-window-restore'
               "
               dense
               round
               size="xs"
-              @click="pageDialog.maximized = !pageDialog.maximized"
+              @click="handleMaximize"
             >
               <q-tooltip
               >{{
-                  !pageDialog.maximized
+                  !props.maximized
                     ? $t("labels.maximizeWindow")
                     : $t("labels.restoreWindow")
                 }}
               </q-tooltip>
             </q-btn>
             <q-btn
-              v-if="pageDialog.closeBtn"
+              v-if="props.buttons.includes('close')"
               v-close-popup
               class="bg-negative text-white"
               dense
@@ -63,14 +63,14 @@
         <q-card-section class="q-py-xs q-px-sm">
           <q-scroll-area
             :style="`${
-              !pageDialog.maximized
+              !props.maximized
                 ? 'height: calc(100vh - 90px)'
                 : 'height: calc(100vh - 45px)'
             }`"
           >
-            <div class="full-width q-px-sm q-py-none">
-              <slot v-if="!loading">
-                <LoaderPage :show="loading"></LoaderPage>
+            <div class="q-px-sm q-py-none flex flex-center">
+              <slot>
+
               </slot>
             </div>
           </q-scroll-area>
@@ -81,37 +81,75 @@
 </template>
 
 <script setup>
-import {computed, defineAsyncComponent, onBeforeMount, ref, watch} from "vue";
-import {storeToRefs} from "pinia";
-import {useQuasar} from "quasar";
+import {onBeforeMount, ref, watch} from "vue";
 import {$t} from "src/services/i18n";
-import {useApp} from "src/composables/useApp";
-import LoaderPage from "src/components/base/LoaderPage.vue";
 
-const $q = useQuasar();
-const $app = useApp();
-const {pageDialog, loading} = $app;
+const props = defineProps({
+  show: {
+    type: Boolean,
+    default: false,
+  },
+  persistent: {
+    type: Boolean,
+    default: true,
+  },
+  icon: {
+    type: String,
+    default: "fas fa-edit"
+  },
+  title: {
+    type: String,
+    default: "a"
+  },
+  position: {
+    type: String,
+    default: "standard"
+  },
+  maximizedBtn: {
+    type: Boolean,
+    default: true
+  },
+  maximized: {
+    type: Boolean,
+    default: false
+  },
+  buttons: {type: Array, default: () => ['close', 'maximize']}
 
-let pageContent = pageDialog.pageComponent;
 
-const emits = defineEmits(["close", "submitForm"]);
+})
+const showDialog = ref(false);
+const isMaximized = ref(false);
+const emits = defineEmits(["show", "hide", 'maximize', "submitForm"]);
 
 onBeforeMount(() => {
-  if (pageDialog.pageComponent) pageContent.value = pageDialog.pageComponent;
+  showDialog.value = props.show
+  isMaximized.value = props.maximized
 });
 
 /**
  * Metodos
  */
 function handleClose() {
-  emits("close");
+  emits("hide");
+}
+
+function handleMaximize() {
+  console.log({max: isMaximized.value})
+  isMaximized.value = !isMaximized.value
 }
 
 function handleFormSubmit(formData) {
   emits("submitForm", formData ?? null);
 }
 
+watch(() => props.show, (newValue) => {
+  showDialog.value = props.show
+})
+
+
 defineExpose({handleClose, handleFormSubmit});
+
+
 </script>
 
 <style lang="scss" scoped>
---
+++ src/components/base/QDialogForm.vue
@@ -0,0 +1,125 @@
+<template>
+  <q-dialog v-model="showDialog" :position="props.position" :persistent="props.persistent"
+            :maximized="maximized"
+            @hide="()=>emitAction('hide', {action: 'hide', data: false})"
+
+  >
+    <q-card
+      :style="!maximized? 'min-width: calc(100vw * 0.7)':'min-width: calc(100vw )'"
+    >
+      <q-card-section v-if="props.title" style="height: 50px; min-height: 50px; max-height: 50px">
+        <div class="text-body1 text-uppercase text-weight-regular">
+          <q-icon :name="props.icon" size="24px"/>
+          {{ title }}
+        </div>
+      </q-card-section>
+      <q-separator/>
+      <q-card-section class="scroll q-pl-md q-pr-sm">
+        <q-scroll-area :class="maximized? 'maximized': 'no-maximized'"
+                       :thumb-style="thumbStyle"
+                       :bar-style="barStyle"
+        >
+          <slot></slot>
+        </q-scroll-area>
+      </q-card-section>
+      <q-separator/>
+      <q-card-actions align="right" style="height: 60px; max-height: 60px; min-height: 60px">
+        <q-btn flat :label="$t('labels.cancel')" color="negative" v-close-popup/>
+        <q-btn v-for="(action, index) in actions" :key="index" flat :label="$t(action.label)" color="primary"
+               @click="action.handler ?? null"/>
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup>
+import {onBeforeMount, ref, watch} from "vue";
+import {$t} from "src/services/i18n";
+
+defineOptions({
+  name: "DarkSwitcher",
+});
+
+const props = defineProps({
+  show: {
+    type: Boolean,
+    default: false,
+  },
+  persistent: {
+    type: Boolean,
+    default: true,
+  },
+  icon: {
+    type: String,
+    default: "fas fa-edit"
+  },
+  title: {
+    type: String,
+    default: ""
+  },
+  position: {
+    type: String,
+    default: "standard"
+  },
+
+  isMaximized: {
+    type: Boolean,
+    default: false
+  },
+
+  actions: {
+    type: Array,
+    default: () => [{
+      action: 'submit', label: 'labels.accept', handler: () => {
+      }
+    }]
+  }
+})
+
+const emits = defineEmits(['action']);
+const maximized = ref(false)
+const showDialog = ref(false)
+
+const thumbStyle = {
+  right: '4px',
+  borderRadius: '5px',
+  backgroundColor: '#8436a8e9',
+  width: '5px',
+  opacity: 0.75
+}
+
+const barStyle = {
+  right: '2px',
+  borderRadius: '9px',
+  backgroundColor: '#3c3c3c',
+  width: '9px',
+  opacity: 0.2
+}
+
+const emitAction = (action, data = null) => {
+  if (action === 'hide') showDialog.value = false
+  emits('action', data)
+}
+
+watch(() => props.show, (newValue) => {
+  showDialog.value = newValue;
+})
+
+onBeforeMount(() => {
+  showDialog.value = props.show
+  maximized.value = props.isMaximized
+})
+
+</script>
+
+<style lang="scss" scoped>
+.no-maximized {
+  height: calc(100vh - 220px);
+  min-width: calc(100vw * 0.1);
+}
+
+.maximized {
+  height: calc(100vh - 160px);
+  min-width: 100vw;
+}
+</style>
