07d88de0f121fa9e8db73a97889242a3c528bbff
d4ism3l <d4ism3l@gmail.com>
2024-08-25 01:47:51 -0400

    feat: Se modifica componente para adicionar funcionalidades y mejorar diseño, tambien se corrige valor emitido.

--- src/components/base/form/DateField.vue
+++ src/components/base/form/DateField.vue
@@ -1,62 +1,14 @@
-<template>
-  <q-input
-    filled
-    v-model="model"
-    :rules="rules"
-    :name="props.name"
-    :readonly="props.readonly"
-    :label="props.label"
-    v-bind="fieldOptions"
-    class="full-width"
-    @update:model-value="onUpdate"
-  >
-    <template v-slot:append>
-      <q-icon name="event" class="cursor-pointer">
-        <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-          <q-date v-model="proxy" mask="DD/MM/YYYY">
-            <div class="row items-center justify-end q-gutter-sm">
-              <q-btn-component
-                icon="check"
-                v-close-popup
-                :tooltips="$q.lang.label.ok"
-                @click="ok(proxy)"
-                v-if="proxy"
-              />
-              <q-btn-component
-                icon="mdi-cancel"
-                color="brown-5"
-                :tooltips="$q.lang.label.cancel"
-                v-close-popup
-              />
-
-              <q-btn-component
-                icon="mdi-eraser"
-                color="red"
-                :tooltips="$q.lang.label.clear"
-                v-close-popup
-                @click="clear"
-                v-if="proxy"
-              />
-            </div>
-          </q-date>
-        </q-popup-proxy>
-      </q-icon>
-    </template>
-  </q-input>
-</template>
-
 <script setup>
-import {computed, onBeforeMount, onMounted, ref} from "vue";
 import {forms} from "src/config/theme/forms";
-import {required} from "src/services/validators";
-import QBtnComponent from "../QBtnComponent.vue";
+import {useQuasar, date} from "quasar";
+import {onBeforeMount, ref} from "vue";
+import QBtnComponent from "components/base/QBtnComponent.vue";
+import {useApp} from "src/composables/useApp";
 
-defineOptions({
-  name: "DateField",
-});
+const $q = useQuasar();
+const {locale, dateLocale} = useApp();
 
 const props = defineProps({
-  modelValue: String,
   name: {
     type: String,
     required: true,
@@ -69,55 +21,101 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  datetime: {
+    type: Boolean,
+    default: false,
+  },
   options: {
     type: Object,
     default: () => ({}),
   },
-});
+  modelValue: {
+    type: String,
+    required: false
+  },
+
+
+})
 
 const emits = defineEmits(["update"]);
 
-const refEl = ref();
-const model = ref(null);
-const proxy = ref(null);
-const rules = ref([]);
+
+const modelInputValue = ref('')
+const modelDate = ref(null)
+const modelTime = ref(null)
+const refInput = ref(null)
+const refDate = ref(null)
+const refTime = ref(null)
+const datePop = ref(false)
+const rules = ref([])
 const fieldOptions = {...forms.date, ...props.options};
-const fieldRules = {...rules.value, ...(props.options?.rules ?? [])};
+const fieldRules = [...rules.value, ...(props.options?.rules ?? [])];
 
-onBeforeMount(() => {
-  if (props.field && props.field?.options) {
-    if (props.options?.required) {
-      rules.value.push(required);
-    }
+const dateNow = () => {
+  if (datetime)
+    return new Date().toISOString().slice(0, 19).replace("T", " ");
+  else
+    return new Date().toISOString().split("T")[0];
+}
+const timeNow = () => {
+  const dateTimeNow = Date.now();
+  return date.formatDate(dateTimeNow, 'HH:mm:ss')
+}
+
+const formatDate = (dateValue) => {
+  let format = 'YYYY-MM-DD HH:mm:ss';
+  if (props.datetime && locale.value === 'es')
+    format = 'DD-MM-YYYY HH:mm:ss';
+  if (!props.datetime && locale.value === 'es')
+    format = 'DD-MM-YYYY';
+  if (!props.datetime && locale.value !== 'es')
+    format = 'YYYY-MM-DD';
+  return date.formatDate(dateValue, format)
+}
+
+
+const ok = () => {
+  if (!modelTime.value && props.datetime) {
+    refTime.value.focus();
+    return;
   }
-});
-
-onMounted(() => {
-  model.value = props.modelValue;
-});
-
-function onUpdate(val) {
-  emits("update", props.name, val);
-}
-
-function validate() {
-  refEl.value.validate();
-}
-
-function resetValidation() {
-  refEl.value.resetValidation();
-}
-
-const ok = (val) => {
-  model.value = val;
-  emits("update", props.name, val);
+  modelInputValue.value = props.datetime ? formatDate(`${modelDate.value.includes('T') ? modelDate.value.split('T')[0] : modelDate.value} ${modelTime.value || '00:00:00'}`)
+    : formatDate(`${modelDate.value}`);
+  const emitDate = props.datetime
+    ? date.formatDate(`${modelDate.value} ${modelTime.value}:00`, 'YYYY-MM-DD HH:mm:ss')
+    : date.formatDate(`${modelDate.value}`, 'YYYY-MM-DD');
+  emits("update", emitDate);
 };
 
 const clear = () => {
-  proxy.value = null;
-  model.value = null;
+  modelInputValue.value = null;
+  modelDate.value = null;
+  modelTime.value = null;
   emits("update", props.name, null);
 };
+
+onBeforeMount(() => {
+  if (props.datetime)
+    modelTime.value = timeNow()
+  if (props.modelValue) {
+    modelDate.value = props.modelValue.split(' ')[0] + 'T00:00:00';
+    if (props.datetime) {
+      modelTime.value = props.modelValue.split(' ')[1];
+    }
+    ok()
+  }
+
+})
+const validate = () => {
+  if (props.datetime) {
+    return date.isValid(`${modelDate.value} ${modelTime.value}`)
+  }
+  return date.isValid(`${modelDate.value}T00:00:00`)
+}
+const resetValidation = () => {
+  refInput.value.resetValidation()
+}
+
 
 defineExpose({
   validate,
@@ -125,4 +123,58 @@ defineExpose({
 });
 </script>
 
-<style scoped lang="scss"></style>
+<template>
+  <q-input ref="refInput" v-model="modelInputValue" :rules="fieldRules" class="full-width"
+           readonly v-bind="fieldOptions"
+           @click="datePop=!datePop"
+  >
+    <template v-slot:append>
+      <q-icon name="event" class="cursor-pointer">
+        <q-popup-proxy :cover="$q.screen.lt.lg" transition-show="scale"
+                       transition-hide="scale" :model-value="datePop"
+                       @hide="datePop=false">
+          <q-date ref="refDate" v-model="modelDate" mask="YYYY-MM-DD"
+                  @update:model-value="ok" :locale="dateLocale.date">
+            <q-bar class="q-pa-sm text-red-3" style="border-radius: 10px">
+              <q-input ref="refTime" borderless
+                       v-if="props.datetime"
+                       v-model="modelTime"
+                       type="time" @update:model-value="ok"/>
+              <q-space/>
+              <div class="q-gutter no-margin q-gutter-xs ">
+                <q-btn-component
+                  icon="check"
+                  v-close-popup
+                  :tooltips="dateLocale.label.ok"
+                  @click="ok"
+                  v-if="modelDate"
+                />
+                <q-btn-component
+                  icon="mdi-cancel"
+                  color="brown-5"
+                  :tooltips="dateLocale.label.cancel"
+                  v-close-popup
+                />
+
+                <q-btn-component
+                  icon="mdi-eraser"
+                  color="red"
+                  :tooltips="dateLocale.label.clear"
+                  v-close-popup
+                  @click="clear"
+                  v-if="modelDate || modelTime"
+                />
+              </div>
+            </q-bar>
+
+          </q-date>
+        </q-popup-proxy>
+      </q-icon>
+    </template>
+  </q-input>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
